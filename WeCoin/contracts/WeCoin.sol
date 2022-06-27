2d2d31b3deb0ce011d99d063edccaa5e43de6b7f
"Chris" <"chris@socialchicken.net">
2022-06-27 16:03:48 +0200

    WeCoinCommit

---
+++ WeCoin/contracts/WeCoin.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract WeCoin is ERC20 {
+
+   
+constructor(uint256 initialSupply) ERC20("Wecoin", "WEC")  {
+     _mint(msg.sender, initialSupply);
+
+    }
+  
+}
+
